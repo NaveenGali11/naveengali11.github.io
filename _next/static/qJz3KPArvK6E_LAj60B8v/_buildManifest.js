95cf3dae36da853e6a9a614a99964b68141be529
NaveenGali11 <68782156+NaveenGali11@users.noreply.github.com>
2023-06-14 06:44:20 +0000

    Deploying to gh-pages from @ NaveenGali11/naveengali11.github.io@61f998969718841463c550b2e3f14492a680b968 🚀

--- _next/static/qJz3KPArvK6E_LAj60B8v/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-7d8a3513086b62c7f397.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
