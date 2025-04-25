4875b107f17c0db30fb463e66407813176f78940
Naveen Gali <naveengali80@gmail.com>
2025-04-25 14:20:24 -0500

    HotJar Setup Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from "next";
 import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import GoogleAnalytics from "./analytics/google-analytics";
+import HotjarAnalytics from "@/app/analytics/hotjar-analytics";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -88,6 +89,7 @@ export default function RootLayout({
         <body
             className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+        <HotjarAnalytics/>
         <GoogleAnalytics/>
         {children}
         </body>
