674c3a7c35cc3b44d375be81fa4c75146c78484d
Naveen Gali <naveengali80@gmail.com>
2025-04-25 14:11:29 -0500

    HotJar Setup

---
+++ app/analytics/hotjar-analytics.tsx
@@ -0,0 +1,23 @@
+"use client";
+import Script from "next/script";
+
+export default function HotjarAnalytics() {
+
+    return (
+        <>
+            <Script id='' strategy='lazyOnload'>
+                {
+                    `
+                    (function(h,o,t,j,a,r){
+                        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                        h._hjSettings={hjid:${process.env.NEXT_PUBLIC_HOTJAR_ID},hjsv:6};
+                        a=o.getElementsByTagName('head')[0];
+                        r=o.createElement('script');r.async=1;
+                        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                        a.appendChild(r);
+                    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+                }
+            </Script>
+        </>
+    );
+}
